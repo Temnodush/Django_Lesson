b19faba6db059fba918ae576f62970c7c4d42584
Temnodush <temnodush@vk.com>
2026-07-03 20:52:53 +0500

    сделаль

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
     path('products/', views.ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    # path('', views.home, name='home'),          # главная страница
-    # path('contacts/', views.contacts, name='contacts'),  # страница контактов
-    # path('products/<int:pk>/', views.product_detail, name='product_detail'), # страница продукта
-    # path('product_list/', views.product_list, name='product_list'), # список продуктов
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/<pk>/edit/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<pk>/delete/', views.ProductDeleteView.as_view(), name='product_confirm_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+
 from .models import Product
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from catalog.forms import ProductForm
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
+
 
 class HomeListView(ListView):
     model = Product
@@ -24,29 +46,3 @@ class ProductDetailView(DetailView):
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
 
-
-
-# def home(request):
-#     """Контроллер для главной страницы"""
-#     products = Product.objects.all()[:3]
-#     context = {'products': products}
-#     return render(request, 'catalog/home.html', context)
-#
-# def contacts(request):
-#     """Контроллер для страницы контактов"""
-#     return render(request, 'catalog/contacts.html')
-#
-# def product_list(request):
-#     products = Product.objects.all()
-#     context = {
-#         'products': products,
-#     }
-#     return render(request, 'catalog/product_list.html', context)
-#
-#
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {
-#         'product': product,
-#     }
-#     return render(request, 'catalog/product_detail.html', context)
